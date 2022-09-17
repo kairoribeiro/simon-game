82bc151ceb020b16ed589bdddc85969e14e6f41d
Kairo Ribeiro <kairo10@gmail.com>
2022-09-17 11:03:46 -0400

    add animation and sound

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 
-gamePattern = [];
 buttonColours = ["red", "blue", "green", "yellow"];
+gamePattern = [];
+
 
 function nextSequence() { 
+
     let randomNumber = Math.floor(Math.random() * 4);
-    return randomNumber;
+    let randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    let audio = new Audio("sounds/" + randomChosenColour + ".mp3")
+    audio.play();
+
+    // console.log(randomChosenColour);
 }
 
-let randomChosenColour = buttonColours[nextSequence()];
-gamePattern.push(randomChosenColour);
-
-
-console.log(gamePattern);
+// nextSequence();
