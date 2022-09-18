f27808edc82521d2938dbd24900ceb1de18f1a04
Kairo Ribeiro <kairo10@gmail.com>
2022-09-18 10:21:07 -0400

    check user and game sequence

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
-buttonColours = ["red", "blue", "green", "yellow"];
-gamePattern = [];
-userClickedPattern = [];
+let buttonColours = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+let userClickedPattern = [];
+let level = 0;
 
 function playSound(colorName) {
     let audio = new Audio("sounds/" + colorName+ ".mp3")
@@ -22,29 +23,40 @@ $(".btn").click( function() {
     userClickedPattern.push(userChosenColour);
     playSound(userChosenColour);
     animatePress(userChosenColour)
+    checkAnswer(userClickedPattern.length-1);
 });
 
 //   console.log(userClickedPattern);
+
+function checkAnswer(currentLevel) {
+    if(gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        console.log("Success");
+        if(userClickedPattern.length === gamePattern.length) { //check if they completed the sequence
+            setTimeout(function() {
+                nextSequence();
+            }, 1000);
+        }
+    } else {
+        console.log("Wrong");
+    }
+}
 
 
 function nextSequence() { 
     let randomNumber = Math.floor(Math.random() * 4);
     let randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
-
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
     playSound(randomChosenColour);
-    
+    level++;
+    $("#level-title").text("Level " + level);
+    userClickedPattern = [];
 }
 
-$("#start-btn").click( function() {
+
+//start the game
+$("#start-btn").click(function() {
+        $("#level-title").text("Level " + level);
         nextSequence();
     });
 
-
-
-
-
-
-
-// nextSequence();
